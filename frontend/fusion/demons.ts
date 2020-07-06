1ad5337198a06c198d02b1b45bdf5ae2ab333102
nvarg <natvargas97@gmail.com>
2020-07-05 21:32:17 -0700

    added agility field

--- frontend/fusion/demons.ts
+++ frontend/fusion/demons.ts
@@ -1,13 +1,14 @@
 export interface Demon {
-    id: number;
-    name: string;
-    race: string;
-    resistances: string;
-    level: number;
-    health: number;
-    mana: number;
-    strength: number;
-    dexterity: number;
-    magic: number;
-    luck: number;
+  id: number;
+  name: string;
+  race: string;
+  resistances: string;
+  level: number;
+  health: number;
+  mana: number;
+  strength: number;
+  dexterity: number;
+  magic: number;
+  agility: number;
+  luck: number;
 }
